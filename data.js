ab44a395fe1c91cae937c34922d55d778abf2804
Squidward Tentacles <squidwardfinance@gmail.com>
2026-02-18 17:53:48 -0500

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 export const tradingData = {
-  currentDay: 1,
+  currentDay: 2,
   startingBalance: 3000,
   targetBalance: 15000,
   brand: "midlifeonsol",
@@ -19,43 +19,23 @@ export const tradingData = {
   dailyStats: [
     { date: "2026-02-16", balance: 3000, pnl: 0 },
     { date: "2026-02-17", balance: 3030.71, pnl: 30.71 },
+    { date: "2026-02-18", balance: 3059.91, pnl: 29.20 },
   ],
   transactions: [
-    {
-      ticker: "TSLA",
-      type: "BUY",
-      shares: 7.3431,
-      price: 408.54,
-      total: 2999.95,
-      date: "2026-02-17 09:32 AM",
-      link: "https://x.com/midlifeonsol/status/2023767646219673775"
-    },
-    {
-      ticker: "TSLA",
-      type: "SELL",
-      shares: 7.3431,
-      price: 411.1338,
-      total: 3019.00,
-      date: "2026-02-17 09:36 AM",
-      link: "https://x.com/midlifeonsol/status/2023768581851562234"
-    },
-    {
-      ticker: "MSTZ",
-      type: "BUY",
-      shares: 222,
-      price: 13.55,
-      total: 3008.10,
-      date: "2026-02-17 11:51 AM",
-      link: "https://x.com/midlifeonsol/status/2023802578862268691"
-    },
-    {
-      ticker: "MSTZ",
-      type: "SELL",
-      shares: 222,
-      price: 13.60,
-      total: 3019.20,
-      date: "2026-02-17 12:00 PM",
-      link: "https://x.com/midlifeonsol/status/2023804970454409282"
-    },
+    // --- FEB 17 ---
+    { ticker: "TSLA", type: "BUY", shares: 7.3431, price: 408.54, total: 2999.95, date: "2026-02-17 09:32 AM", link: "https://x.com/midlifeonsol/status/2023767646219673775" },
+    { ticker: "TSLA", type: "SELL", shares: 7.3431, price: 411.1338, total: 3019.00, date: "2026-02-17 09:36 AM", link: "https://x.com/midlifeonsol/status/2023768581851562234" },
+    { ticker: "MSTZ", type: "BUY", shares: 222, price: 13.55, total: 3008.10, date: "2026-02-17 11:51 AM", link: "https://x.com/midlifeonsol/status/2023802578862268691" },
+    { ticker: "MSTZ", type: "SELL", shares: 222, price: 13.60, total: 3019.20, date: "2026-02-17 12:00 PM", link: "https://x.com/midlifeonsol/status/2023804970454409282" },
+    // --- FEB 18 ---
+    { ticker: "PANW", type: "BUY", shares: 20, price: 150.4203, total: 3008.41, date: "2026-02-18 08:58 AM", link: "https://x.com/midlifeonsol/status/2024121467286311276" },
+    { ticker: "PANW", type: "SELL", shares: 20, price: 150.75, total: 3015.00, date: "2026-02-18 09:30 AM", link: "https://x.com/midlifeonsol/status/2024129662364848518" },
+    { ticker: "BMNR", type: "BUY", shares: 151, price: 20.0486, total: 3027.34, date: "2026-02-18 09:41 AM", link: "https://x.com/midlifeonsol/status/2024132333809336705" },
+    { ticker: "BMNR", type: "SELL", shares: 151, price: 20.1014, total: 3035.31, date: "2026-02-18 09:56 AM", link: "https://x.com/midlifeonsol/status/2024136191243620488" },
+    { ticker: "HMY", type: "BUY", shares: 150, price: 20.2383, total: 3035.75, date: "2026-02-18 10:02 AM", link: "https://x.com/midlifeonsol/status/2024137574772539863" },
+    { ticker: "HMY", type: "SELL", shares: 150, price: 20.30, total: 3045.00, date: "2026-02-18 10:11 AM", link: "https://x.com/midlifeonsol/status/2024139944269111706" },
+    { ticker: "TSLA", type: "BUY", shares: 7, price: 412.43, total: 2887.01, date: "2026-02-18 10:35 AM", link: "https://x.com/midlifeonsol/status/2024145989926351260" },
+    { ticker: "TSLA", type: "SELL", shares: 7, price: 413.20, total: 2892.40, date: "2026-02-18 10:38 AM", link: "https://x.com/midlifeonsol/status/2024146634523701541" },
+    { ticker: "BMNR", type: "BUY", shares: 148, price: 20.62, total: 3051.76, date: "2026-02-18 10:41 AM", link: "https://x.com/midlifeonsol/status/2024147374663221307" },
   ],
 };
